055fb7145f9f238c730c6f9c94684bf4a2cb6f98
Zulfiyya Huseynova <tu7kswf4d@aztu.local>
2022-06-10 16:03:56 +0400

    add

---
+++ SaasPal Back/SaaSpal/SaaSpal/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SaaSpal.DAL;
+using SaaSpal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaaSpal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            List<Slider> sliders = await _context.Sliders.ToListAsync();
+            return View(sliders);
+        }
+    }
+}
---
+++ SaasPal Back/SaaSpal/SaaSpal/Models/Slider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaaSpal.Models
+{
+    public class Slider
+    {
+        public int Id { get; set; }
+        public string Image { get; set; }
+        public string Name { get; set; }
+        public string Comment { get; set; }
+    }
+}
